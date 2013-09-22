07ffdc8493c6a7de08182349b36ccf1cabb190eb
Hernán Carrizo <owentar@gmail.com>
2013-09-22 03:50:56 -0300

    add markers to map

--- src/main/java/com/whiterabbit/bondi/client/application/map/MapView.java
+++ src/main/java/com/whiterabbit/bondi/client/application/map/MapView.java
@@ -1,12 +1,15 @@
 package com.whiterabbit.bondi.client.application.map;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gwt.maps.client.LoadApi;
 import com.google.gwt.maps.client.LoadApi.LoadLibrary;
 import com.google.gwt.maps.client.MapOptions;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.base.LatLng;
+import com.google.gwt.maps.client.overlays.Marker;
+import com.google.gwt.maps.client.overlays.MarkerOptions;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.HTMLPanel;
@@ -14,6 +17,7 @@ import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.whiterabbit.bondi.client.constants.MapsConstants;
+import com.whiterabbit.bondi.shared.dto.Location;
 
 public class MapView extends ViewImpl implements MapPresenter.MyView {
 
@@ -25,22 +29,49 @@ public class MapView extends ViewImpl implements MapPresenter.MyView {
 
     private final MapsConstants mapsConstants;
     private MapWidget map;
+    private List<Marker> markers;
 
     @Inject
     public MapView(final Binder binder, final MapsConstants mapsConstants) {
         this.mapsConstants = mapsConstants;
+        this.markers = new ArrayList<Marker>();
         initWidget(binder.createAndBindUi(this));
         loadMapsAPI();
     }
 
     @Override
     public void setUp() {
-        MapOptions options = createMapOptions(mapsConstants.defaultLatitude(), mapsConstants.defaultLongitude(),
-                mapsConstants.defaultZoom());
+        MapOptions options = createMapOptions(mapsConstants.defaultLatitude(),
+                mapsConstants.defaultLongitude(), mapsConstants.defaultZoom());
         map.setOptions(options);
     }
 
-    private MapOptions createMapOptions(double latitude, double longitude, int zoom) {
+    @Override
+    public void showLocations(List<Location> locations) {
+        for (Location location : locations) {
+            LatLng latlng = LatLng.newInstance(location.getLatitude(),
+                    location.getLongitude());
+            final MarkerOptions options = MarkerOptions.newInstance();
+            options.setPosition(latlng);
+            options.setMap(map);
+
+            final Marker marker = Marker.newInstance(options);
+
+            markers.add(marker);
+        }
+    }
+
+    @Override
+    public void clearLocations() {
+        for (Marker marker : markers) {
+            marker.clear();
+        }
+
+        markers.clear();
+    }
+
+    private MapOptions createMapOptions(double latitude, double longitude,
+            int zoom) {
         LatLng mainLocation = LatLng.newInstance(latitude, longitude);
         MapOptions options = MapOptions.newInstance();
         options.setCenter(mainLocation);
@@ -70,7 +101,9 @@ public class MapView extends ViewImpl implements MapPresenter.MyView {
     }
 
     private MapWidget createMap() {
-        LatLng mainLocation = LatLng.newInstance(mapsConstants.defaultLatitude(), mapsConstants.defaultLongitude());
+        LatLng mainLocation = LatLng.newInstance(
+                mapsConstants.defaultLatitude(),
+                mapsConstants.defaultLongitude());
         MapOptions options = MapOptions.newInstance();
         options.setCenter(mainLocation);
         options.setZoom(mapsConstants.defaultZoom());
